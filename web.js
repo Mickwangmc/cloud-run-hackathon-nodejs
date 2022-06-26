8d35bcb7acaf0ccdc1a09bf84e50ff0b2c43ade1
Mick <>
2022-06-26 23:59:26 +0800

    Chase mode is ready

--- web.js
+++ web.js
@@ -88,13 +88,18 @@ app.post('/', function (req, res) {
   let hasTargetInFront = false;
   let targetDirection = [];  // possible targets
   let hinderDirection = getInitialHinder(myX, myY, dims[0], dims[1]); // hinder my fallback
+  let highestScorePlayer = ["", 0, 0, 0]; // [playerId, score, positionX, positionY]
 
   console.log(`> myX: ${myX}, myY: ${myY}, myDirection: ${myDirection}, wasHit: ${wasHit}`)
   console.log(`> myRightDirection: ${myRightDirection}, myLeftDirection: ${myLeftDirection}, myBasicMove: ${myBasicMove}`)
 
   // 1. Check if any one in my range and direction
   enemyIds.forEach(id => {
-    const { x: targetX, y: targetY } = state[id];
+    const { x: targetX, y: targetY, score } = state[id];
+
+    if (score > highestScorePlayer[1]) {
+      highestScorePlayer = [id, score, targetX, targetY];
+    }
 
     if (myY > targetY && (myY - targetY <= 3) && myX === targetX) {
       // "N"
@@ -121,52 +126,118 @@ app.post('/', function (req, res) {
   console.log(`>> hasTargetInFront: ${hasTargetInFront}`)
   console.log(`>> targetDirection: ${targetDirection.join(",")}`);
   console.log(`>> hinderDirection: ${hinderDirection.join(",")}`);
+  console.log(`>> highestScorePlayer is ${highestScorePlayer[0]}, score: ${highestScorePlayer[1]}, position: (${highestScorePlayer[2]}, ${highestScorePlayer[3]})`)
 
-  // 2. Decide run or hit
-  if (wasHit) {
-    // 2-1. Now is under attack
-    if (hinderDirection.length === DIRECTIONS.length) {
-      // 2-1-1. Blocked every where, let's hit
-      res.send("T");
-      return;
-    } else if (hinderDirection.includes(myDirection)) {
-      // 2-1-2. FRONT is blocked, find other way to run
-      // If RIGHT hand side is blocked, TURN LEFT, otherwitse TURN RIGHT
-      res.send(hinderDirection.includes(myRightDirection) ? "L" : "R")
-      return;
+  // Normal
+  // // 2. Decide run or hit
+  // if (wasHit) {
+  //   // 2-1. Now is under attack
+  //   if (hinderDirection.length === DIRECTIONS.length) {
+  //     // 2-1-1. Blocked every where, let's hit
+  //     res.send("T");
+  //     return;
+  //   } else if (hinderDirection.includes(myDirection)) {
+  //     // 2-1-2. FRONT is blocked, find other way to run
+  //     // If RIGHT hand side is blocked, TURN LEFT, otherwitse TURN RIGHT
+  //     res.send(hinderDirection.includes(myRightDirection) ? "L" : "R")
+  //     return;
+  //   } else {
+  //     // 2-1-3. FRONT is clear, adopt basic move.
+  //     res.send(myBasicMove);
+  //     return;
+  //   }
+  // }
+
+  // if (hasTargetInFront) {
+  //   // 2-2. Now is not under attack, if has any target in FRONT, hit!
+  //   res.send("T");
+  //   return;
+  // }
+
+  // if (targetDirection.length > 0) {
+  //   // 2-3. No target in FRONT, but other direction has
+  //   if (targetDirection.includes(myRightDirection)) {
+  //     // RIGHT has
+  //     res.send("R");
+  //     return;
+  //   } else {
+  //     // (this includes targetDirection.includes(myLeftDirection) )
+  //     // LEFT or BACK has
+  //     res.send("L");
+  //     return;
+  //   }
+  // }
+
+  // // 3. No target in any direction, basic move
+  // res.send(myBasicMove);
+
+  // Chase Mode
+  if (highestScorePlayer[2] === myX) {
+    // On the same X axis
+    if (myY > highestScorePlayer[3]) {
+      // On North side
+      if (myDirection === "N") {
+        // Hit if in hit range, or move FORWARD
+        res.send(myY - highestScorePlayer[3] <= 3 ? "T" : "F");
+      } else {
+        res.send(myDirection === "E" ? myLeftDirection : myRightDirection);
+      }
     } else {
-      // 2-1-3. FRONT is clear, adopt basic move.
-      res.send(myBasicMove);
-      return;
+      // On South side
+      if (myDirection === "S") {
+        // Hit if in hit range, or move FORWARD
+        res.send(highestScorePlayer[3] - myY <= 3 ? "T" : "F");
+      } else {
+        res.send(myDirection === "E" ? myRightDirection : myLeftDirection);
+      }
+    }
+  } else if (highestScorePlayer[3] === myY) {
+    // On the same Y axis
+    if (highestScorePlayer[2] > myX) {
+      // On East side
+      if (myDirection === "E") {
+        // Hit if in hit range, or move FORWARD
+        res.send(highestScorePlayer[2] - myX <= 3 ? "T" : "F");
+      } else {
+        res.send(myDirection === "N" ? myRightDirection : myLeftDirection);
+      }
+    } else {
+      // On West side
+      if (myDirection === "W") {
+        // Hit if in hit range, or move FORWARD
+        res.send(myX - highestScorePlayer[2] <= 3 ? "T" : "F");
+      } else {
+        res.send(myDirection === "N" ? myLeftDirection : myRightDirection);
+      }
+    }
+  } else {
+    // In one of quadrants, move depends on my current direction
+    if (myDirection === "N") {
+      if (highestScorePlayer[3] < myY && !hinderDirection.includes("N")) {
+        res.send("F");
+      } else {
+        res.send(hinderDirection.includes(myLeftDirection) ? "R" : "L");
+      }
+    } else if (myDirection === "E") {
+      if (highestScorePlayer[2] > myX && !hinderDirection.includes("E")) {
+        res.send("F");
+      } else {
+        res.send(hinderDirection.includes(myLeftDirection) ? "R" : "L");
+      }
+    } else if (myDirection === "S") {
+      if (highestScorePlayer[3] > myY && !hinderDirection.includes("S")) {
+        res.send("F");
+      } else {
+        res.send(hinderDirection.includes(myLeftDirection) ? "R" : "L");
+      }
+    } else if (myDirection === "W") {
+      if (highestScorePlayer[2] < myX && !hinderDirection.includes("W")) {
+        res.send("F");
+      } else {
+        res.send(hinderDirection.includes(myLeftDirection) ? "R" : "L");
+      }
     }
   }
-
-  if (hasTargetInFront) {
-    // 2-2. Now is not under attack, if has any target in FRONT, hit!
-    res.send("T");
-    return;
-  }
-
-  if (targetDirection.length > 0) {
-    // 2-3. No target in FRONT, but other direction has
-    if (targetDirection.includes(myRightDirection)) {
-      // RIGHT has
-      res.send("R");
-      return;
-    } else {
-      // (this includes targetDirection.includes(myLeftDirection) )
-      // LEFT or BACK has
-      res.send("L");
-      return;
-    }
-  }
-
-  // 3. No target in any direction, basic move
-  // 4. Maybe could chase the nearest one?
-  res.send(myBasicMove);
-
-  // const moves = ['F', 'L', 'R'];
-  // res.send(moves[Math.floor(Math.random() * moves.length)]);
 });
 
 app.listen(process.env.PORT || 8080);
